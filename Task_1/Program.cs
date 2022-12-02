6cb5ecaa8072f533fe8dc7e13d1b05b1ca1a1b1b
mzemskov <mzemskov@dimax.digital>
2022-12-02 19:23:31 +0300

    task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int n = 2;
+int l = a; //кладем изначальное значение в доп переменную
+
+if (b == 0)
+Console.WriteLine(1);
+else
+{
+    while ( n <= b )
+    {
+        a = a * l;
+        n++;
+    }
+Console.WriteLine(a);
+}
