4a06651117416ee0533b19857f3f6ca82da639aa
mzemskov <mzemskov@dimax.digital>
2022-12-04 19:22:33 +0300

    task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int counter = Convert.ToString(n).Length;
+int i = 0;
+int sum = 0;
+
+while (i < counter)
+{
+sum = sum + n % 10;
+n = n / 10;
+i++;
+}
+
+	Console.WriteLine($"Сумма цифр в числе N = {sum}");
+
